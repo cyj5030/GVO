50998c5840d634124c6fdde44e86940daf8b3f6a
Yijun Cao <yijuncaoo@gmail.com>
2023-04-01 15:30:27 +0800

    fix bug

--- evaluation/__init__.py
+++ evaluation/__init__.py
@@ -2,6 +2,4 @@ from .eval_flow import eval_kitti_flow
 from .utils import flow_to_image
 from .eval_depth import eval_kitti_depth
 from .eval_odometry_kitti import eval_kitti_odometry
-from .eval_odometry_tmu import eval_tmu_odometry
-from .eval_odometry_euroc import eval_euroc_odometry
 from .eval_odometry_kaist import eval_kaist_odometry
